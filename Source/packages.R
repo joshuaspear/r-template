c4fed994bf629590b53a8c3e4e88db41ba83067e
Lucy Karwatowska <lucy.karwatowska.18@ucl.ac.uk>
2021-11-29 15:35:01 +0000

    deleted packages from Source folder

--- Source/packages.R
+++ Source/packages.R
@@ -2,37 +2,6 @@
 #### Install and load relevant packages
 
 #### Install relevant packages (if applicable) ####
-install.packages("dplyr") # for data manipulation
-install.packages("metafor") # to do meta-analysis
-install.packages("compute.es") # for effect size conversions
-install.packages("tidyverse") # data management and manipulation 
-install.packages("devtools") # to install packages from Git
-install.packages("gt") # apa tables
-install.packages("PRISMAstatement") # for prisma flow diagram
-devtools::install_github("NightingaleHealth/ggforestplot") # for forest plots
-install.packages("htmltools") # to save as web page
-install.packages("ggforce") # extra functions for ggplot
-install.packages("ggsci") # scientific journals colour palette 
-install.packages("apaTables") # apa tables
-install.packages("MAd") # for aggregating effect sizes
-install.packages("kableExtra") # tables
-remotes::install_github("wviechtb/metafor") # latest version of meta-analysis package
-install.packages("forestplot") # for forest plots
 
 
 #### Load packages ####
-library(dplyr) # for data manipulation
-library(metafor) # to do meta-analysis
-library(compute.es) # for effect size conversions
-library(tidyverse) # data management and manipulation 
-library(devtools) # to install packages from Git
-library(gt) # apa tables
-library(PRISMAstatement) # for prisma flow diagram
-library(ggforestplot) # for forest plots
-library(htmltools) # to save as web page
-library(ggforce) # extra functions for ggplot
-library(ggsci) # scientific journals colour palette 
-library(apaTables) # apa tables
-library(MAd) # for aggregating effect sizes
-library(kableExtra) # tables
-library(forestplot) # for forest plots
